73c9deeaf93c3e8a4057f4ab4d38a737e5129ebc
blahpetr <pblaha1jr@gmail.com>
2026-03-03 23:33:53 +0100

    Can now open a file

--- main.asm
+++ main.asm
@@ -8,6 +8,15 @@ numberOfFATs equ 0x7C10
 rootEntryCount equ 0x7C11
 sectorsPerFAT equ 0x7C16
 
+;Layout info
+rootDirStartSector equ 0x6000 ;4 bytes
+rootDirOffset equ 0x6004 ;4 bytes
+rootDirSizeBytes equ 0x6008 ;4 bytes
+rootDirSectors equ 0x600C ;4 bytes
+firstDataSector equ 0x6010 ;4 bytes
+fatOffset equ 0x6014 ;4 bytes
+curentScanOffset equ 0x6018 ; 2 bytes
+
 .draw_CLI:
 	mov ah, 0x00
 	mov [string_size], ah ;Input index
@@ -50,36 +59,6 @@ sectorsPerFAT equ 0x7C16
 	int 0x10		; Move carry
 
 	mov cx, 0x0000
-
-.print:
-
-	cmp cl, [string_size]
-	jz .done_print
-
-	;mov ah, 0x0E
-	;mov al, "#" ; Debug, remember to remove this!
-	;int 0x10
-
-	mov bx, 0x0500
-	add bx, cx
-	add bx, 1
-	mov al, [bx]
-	mov ah, 0x0E
-	int 0x10
-
-	inc cl
-	jmp .print
-
-.done_print:
-	mov ah, 0x0E
-	mov al, 0x0D
-	int 0x10
-
-	mov ah, 0x0E
-	mov al, 0x0A ; Move carry
-	int 0x10
-	
-	jmp .draw_CLI
 
 .run_prg:
 	xor eax, eax
@@ -142,6 +121,7 @@ sectorsPerFAT equ 0x7C16
 	mov [0x9004], ax
 	mov [0x9012], eax			; first part of LBA (0 since disk too small)
 	mov ax, 0x8000				; where to save es offset (0x8000)
+	mov [curentScanOffset], ax
 	mov [0x9006], ax
 	mov eax, rootDirOffset
 	mov ebx, 0x0200				; second part of LBA, calculated as diskOffset/512
@@ -153,11 +133,56 @@ sectorsPerFAT equ 0x7C16
 	mov si, 0x9000
 	int 0x13		; Request read from disk
 	jc .fail_brokenDisk
-
+	
 .find_file:
-	xor eax, eax
-	xor ebx, ebx
+	mov bx, [curentScanOffset]
+	mov eax, [bx]
+	mov ebx, [bx+4]
+	mov ecx, [targetName]
+	mov edx, [targetName+4]
 
+	cmp ax, 0x0000
+	je .fail_fileNotFound
+	
+	cmp eax, ecx
+	jne .incorrectName
+
+	cmp ebx, edx
+	jne .incorrectName
+
+	xor eax, eax
+	mov bx, [curentScanOffset]
+	mov ax, [bx+26]
+
+	sub ax, 0x0002
+	xor bx, bx
+	mov bl, [sectorsPerCluster]
+	mul bx
+	mov ebx, [firstDataSector]
+	add eax, ebx
+
+	mov [0x9008], eax 
+	mov al, 0x10				; DAP size
+	mov [0x9000], al			
+	mov al, 0x00 				; reserved 0
+	mov [0x9001], al
+	mov ax, 0x0001				; how many sectors to load (1 sector usually 512 bytes)
+	mov [0x9002], ax
+	xor eax, eax				; where to save BX segment (0)
+	mov [0x9004], ax
+	mov [0x9012], eax			; first part of LBA (0 since disk too small)
+	mov ax, 0x8000				; where to save es offset (0x8000)
+	mov [0x9006], ax
+								; Create the DAP
+
+	mov ah, 0x42
+	mov dl, 0x80
+	mov si, 0x9000
+	int 0x13		; Request read from disk
+	jc .fail_brokenDisk
+
+	mov ax, 0x8000
+	jmp ax
 
 .incorrectName:
 	xor eax, eax
@@ -173,31 +198,6 @@ sectorsPerFAT equ 0x7C16
 
 .fail_brokenDisk:
 	jmp .draw_CLI
-	
-;Layout info
-rootDirStartSector db 0x00000000 ;4 bytes
-rootDirOffset db 0x00000000 ;4 bytes
-rootDirSizeBytes db 0x00000000 ;4 bytes
-rootDirSectors db 0x00000000 ;4 bytes
-firstDataSector db 0x00000000 ;4 bytes
-fatOffset db 0x00000000 ;4 bytes
-
-;Directory scanning
-entryCount db 0x0000 ;might delete
-entryOffset db 0x00000000 ;4 bytes
-entryFirstByte db 0x00 ;1 byte
-filenameMatch db 0x00 ;1 byte
-
-;File metadata
-firstCluster db 0x0000 ;2 bytes
-fileSize db 0x00000000 ;4 bytes
-currentCluster db 0x0000 ;2 bytes
-nextCluster db 0x0000 ;2 bytes
-
-;Cluster math
-firstSectorOfCluster db 0x00000000 ;4 bytes
-fileDataOffset db 0x00000000 ;4 bytes
-fatEntryOffset db 0x00000000 ;4 bytes
 
 targetName db "HELLO   " ;TODO: Make sure it is connected to the tty
 targetExtension db "BIN"
